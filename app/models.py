ccb52820a95f7e391332030f82534de2be6f4e22
yauheni.semashka <yauheni@craft.co>
2019-04-29 16:54:50 +0300

    Initial model added.

---
+++ app/models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from peewee import *
+from playhouse.postgres_ext import *
+
+
+db = PostgresqlExtDatabase(
+    database='steamdb',
+    user='steamdb',
+    password='steamdb',
+    port=5432,
+    register_hstore=True
+)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Game(BaseModel):
+    class Meta:
+        db_table = 'games'
+
+    appid = IntegerField(primary_key=True)
+    name = CharField()
+    is_free = BooleanField()
+    detailed_description = TextField()
+    about_the_game = TextField()
+    pc_requirements = JSONField()
+    mac_requirements = JSONField()
+    linux_requirements = JSONField()
+    price = DecimalField()
+    price_currency = CharField()
+    windows_compatible = BooleanField()
+    mac_compatible = BooleanField()
+    linux_compatible = BooleanField()
+    genres = JSONField()
+
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField()
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now()
+        return super(Something, self).save(*args, **kwargs)
